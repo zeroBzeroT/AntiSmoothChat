c95c17682f6cb2af224ce95b5d829cc6e50d0252
blockparole <48992448+blockparole@users.noreply.github.com>
2020-09-08 21:55:38 +0200

    fix main class use name from plugin.yml

--- src/main/java/com/krazzzzymonkey/antismoothchat/AntiSmoothChat.java
+++ src/main/java/com/krazzzzymonkey/antismoothchat/AntiSmoothChat.java
@@ -9,7 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin implements Listener {
+public final class AntiSmoothChat extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
